a71720284c0dece89814e8155704746ca4fd6d28
Rohit Khadse <rohit.a.khadse@gmail.com>
2025-09-15 21:22:47 -0400

    Create 700-SearchinaBinarySearchTree

---
+++ 700-SearchinaBinarySearchTree/version/java/700-SearchinaBinarySearchTree_20250915_212247.java
@@ -0,0 +1,29 @@
+// Last updated: 9/15/2025, 9:22:47 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root==null || root.val == val){
+            return root;
+        }
+        
+        if(val<root.val){
+            return searchBST(root.left,val);
+        }
+        
+        return searchBST(root.right,val);
+    }
+}
